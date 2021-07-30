be3f20fa08b81c646540afff95694b3649d02b8c
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-07-30 22:32:48 +0300

    app created

---
+++ src/components/info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Weather Forecast</h2>
+                <p>Check the weather in your city!</p>
+            </div>
+        )
+    }
+}
