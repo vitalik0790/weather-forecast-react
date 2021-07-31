db6b37f23b48b3c6774c11ccdec6c9e262eb0f77
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-07-31 14:11:09 +0300

    api_key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Form from './components/form';
 import Info from './components/info'
+import Weather from './components/weather';
+
+const API_KEY = '04331ebc0b82ae7fed82fb7e0658d99c';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <Info />
+                <Form />
+                <Weather />
             </div>
         )
     }
